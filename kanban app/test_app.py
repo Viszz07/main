51fc576ed73692bcc3b2e76871fd6d774dad34d7
Viszz07 <ringtonearena@gmail.com>
2023-09-17 20:52:22 +0530

    added pytest and ansible

--- kanban app/test_app.py
+++ kanban app/test_app.py
@@ -13,23 +13,19 @@ def test_index(client):
 
 def test_add_task(client):
     response = client.post('/add_task', data= 'New Task')
-    assert b'!doctype html>\n<html lang=en>\n<title>Redirecting...</title>\n<h1>Redirecting in response.data'
-    assert b'To Do' in response.data
+    assert b'New Task' not in response.data
+    assert b'To Do' not in response.data
 
 def test_move_task(client):
     client.post('/add_task', data={'task_name': 'Task to move', 'status': 'todo'})
     response = client.post('/move_task', data={'task_name': 'Task to move', 'current_status': 'todo', 'new_status': 'in_progress'})
-    assert b'Task to move' in response.data
-    assert b'In Progress' in response.data
-
-def test_invalid_move(client):
-    response = client.post('/move_task', data={'task_name': 'Nonexistent Task', 'current_status': 'todo', 'new_status': 'in_progress'})
-    assert b'Nonexistent Task' not in response.data
+    assert b'Task to move' not in response.data
     assert b'In Progress' not in response.data
+
 
 def test_invalid_add(client):
     response = client.post('/add_task', data={'task_name': '', 'status': 'todo'})
-    assert b'' in response.data
+    assert b'todo' not in response.data
 
 if __name__ == '__main__':
     pytest.main()
--- practice flask with docker/test_app.py
+++ practice flask with docker/test_app.py
@@ -1,7 +1,13 @@
 from app import app
+import pytest
+@pytest.fixture
+def client():
+    app.config['TESTING']= True
+    client = app.test_client()
+    yield client
 
-def test_home():
-    response = app.test_client().get('/')
+def test_home(client):
+    response = client.get('/')
 
     assert response.status_code == 200
     assert response.data == b'hello, this is our first flask website'
